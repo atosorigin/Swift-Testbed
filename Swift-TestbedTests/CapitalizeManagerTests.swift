3c3a64d63028b1dd407f6e0c69eeab0aa163f6fd
Mark Fearnley <mark.fearnley@atos.net>
2016-10-20 15:08:45 +0100

    Added basic unit tests

--- Swift-TestbedTests/CapitalizeManagerTests.swift
+++ Swift-TestbedTests/CapitalizeManagerTests.swift
@@ -9,11 +9,15 @@
 import XCTest
 @testable import Swift_Testbed
 
-class Swift_TestbedTests: XCTestCase {
+class CapitalizeManagerTests: XCTestCase {
+    
+    var manager: CapitalizeManager? = nil;
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        manager = CapitalizeManager();
     }
     
     override func tearDown() {
@@ -21,16 +25,10 @@ class Swift_TestbedTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testCapitalizeString() {
+        let result = manager!.capitalizeString(string: "Test String");
+        
+        XCTAssertEqual(result, "TEST STRING");
     }
     
 }
